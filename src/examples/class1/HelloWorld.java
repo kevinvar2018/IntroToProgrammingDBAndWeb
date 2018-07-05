e4a3046f154f262a97ef468f31541d0fd367a6d3
kevinvar2018 <kevinvar2018@gmail.com>
2018-07-05 03:50:20 +0000

    Test commit

--- src/examples/class1/HelloWorld.java
+++ src/examples/class1/HelloWorld.java
@@ -6,7 +6,7 @@ package examples.class1;
  * @version 1.0
  */
 
-public class HelloWorld {
+public class HelloWorld { 
     
     public static void main (String args[]) {
         System.out.println("Hello World!");
